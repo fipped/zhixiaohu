89aa8846df4342ebbf3273fb5f70272ccbf8f84f
flipped <19flipped@gmail.com>
2017-12-23 10:27:01 +0800

    frontend: modify menu

--- frontend/src/views/index.vue
+++ frontend/src/views/index.vue
@@ -1,85 +1,53 @@
 <template>
   <div>
     <div class="layout">
-        <Menu mode="horizontal" :theme="navTheme" active-name="1"> 
-            <div class="layout-logo">知小乎</div>
-            <div class="layout-nav">
-            <MenuItem name="1">
-                <Icon type="ios-people"></Icon>
-                广场
-            </MenuItem>
-            <MenuItem name="2">
-                <Icon type="fireball"></Icon>
-                热点
-            </MenuItem>
-            <MenuItem name="3">
-                <Icon type="leaf"></Icon>
-                最新
-            </MenuItem>
-            <MenuItem name="4">
-                <Icon type="university"></Icon>
-                提问
-            </MenuItem>
+        <TopBar></TopBar>
+        <div class="container">
+            <SubMenuBar></SubMenuBar>
+            <div class="feed-flow">
+                <AnswerListCard></AnswerListCard>
             </div>
-         </Menu>
-        <div class="layout-content">
-          <div class="layout-content-main">Content</div>
         </div>
     </div>
   </div>
 </template>
 
 <script>
-	import cookieManage from '@/mixins/cookieManage'
-	import initInfo from '@/mixins/initInfo'
+  const TopBar = resolve => require(['@/components/topBar'], resolve)
+  const SubMenuBar = resolve => require(['@/components/subMenuBar'], resolve)
+  const AnswerListCard = resolve => require(['@/components/answerListCard'], resolve)
   export default {
-		name: 'IdxPage',
-		mixins: [cookieManage, initInfo],
-    data () {
-        return {
-            navTheme: 'light'
-        }
-		},
-		methods: {
-		},
-		components: {
-		},
-		created () {
-			console.log(this.initInfo())
-		}
+    name: 'IdxPage',
+    components: {TopBar,SubMenuBar,AnswerListCard}
   }
 </script>
 
 <style scoped>
     .layout{
-        float: left;
+        min-width: 960px;
         width: 100vw;
         height: 100vh;
-        border: 1px solid #d7dde4;
         background: #f5f7f9;
     }
-    .layout-logo{
-        width: 100px;
-        height: 35px;
-        border-radius: 3px;
+    .container{
         float: left;
-        position: relative;
-        top: 0;
-        left: 60px;
-        font-size: 30px;
+        width: 600px;
+        margin-left: 160px;
+        margin-top: 10px;
     }
-    .layout-nav{
-        width: 420px;
-        margin: 0 auto;
-    }
-
-    .layout-content{
-        margin: 15px;
-        overflow: hidden;
+    
+    .card{
+        margin-bottom: 10px;
         background: #fff;
-        border-radius: 4px;
+        overflow: hidden;
+        border-radius: 2px;
+        -webkit-box-shadow: 0 1px 3px rgba(0,0,0,.1);
+        box-shadow: 0 1px 3px rgba(0,0,0,.1);
+        -webkit-box-sizing: border-box;
+        box-sizing: border-box;
     }
-    .layout-content-main{
+    .feed-flow .card{
         padding: 10px;
     }
+    
 </style>
