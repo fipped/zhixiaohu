3dc24ec40ac2f7a21410950f5ca9505ea7ba2c97
flipped <19flipped@gmail.com>
2017-12-24 12:05:48 +0800

    frontend: add topBar,toolBar,ListCard

--- frontend/build/webpack.base.conf.js
+++ frontend/build/webpack.base.conf.js
@@ -3,6 +3,7 @@ const path = require('path')
 const utils = require('./utils')
 const config = require('../config')
 const vueLoaderConfig = require('./vue-loader.conf')
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 function resolve(dir) {
   return path.join(__dirname, '..', dir)
@@ -75,7 +76,12 @@ module.exports = {
           limit: 10000,
           name: utils.assetsPath('fonts/[name].[hash:7].[ext]')
         }
+      },
+      {
+        test: /(\.css)$/,
+        loader: ExtractTextPlugin.extract('style-loader', 'css-loader'),
       }
+
     ]
   },
   node: {
