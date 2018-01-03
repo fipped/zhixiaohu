0f3cdc703180a9d76a00ca37f694da32f5f3ce74
GhosKno <ghosknochen@gmail.com>
2018-01-03 15:20:44 +0800

    Add: userCard && modify profile page

--- frontend/src/views/user/profile.vue
+++ frontend/src/views/user/profile.vue
@@ -33,7 +33,7 @@
           <Row class="profile-body">
             <Col span="16" class="profile-body-left">
               <div>
-                <Tabs value="timeline" class="profile-pane">
+                <Tabs :value="profilePaneActiveName" class="profile-pane">
                   <TabPane label="动态" name="timeline">
                     <div class="profile-pane-header">我的动态</div>
                     <div class="profile-pane-body">
@@ -71,7 +71,7 @@
                       >
                       </AnswerListCard>
                     </div>
-                    </TabPane>
+                  </TabPane>
                   <TabPane label="提问" name="question">
                     <div class="profile-pane-header">我的提问</div>
                     <div class="profile-pane-body">
@@ -91,22 +91,84 @@
                       </AnswerListCard>
                     </div>
                   </TabPane>
+                  <TabPane label="更多" name="more">
+                    <Tabs :value="morePaneActiveName" class="morePane">
+                      <TabPane label="我关注的人" name="watch">
+                        <user-card
+                          userName="vczh"
+                          description="专业造轮子，拉黑抢前排。http://gaclib.net"
+                          :answer="17758"
+                          :watched="647935"
+                        ></user-card>
+                        <user-card
+                          userName="vczh"
+                          description="专业造轮子，拉黑抢前排。http://gaclib.net"
+                          :answer="17758"
+                          :watched="647935"
+                        ></user-card>
+                        <user-card
+                          userName="vczh"
+                          description="专业造轮子，拉黑抢前排。http://gaclib.net"
+                          :answer="17758"
+                          :watched="647935"
+                        ></user-card>
+                        <user-card
+                          userName="vczh"
+                          description="专业造轮子，拉黑抢前排。http://gaclib.net"
+                          :answer="17758"
+                          :watched="647935"
+                        ></user-card>
+                      </TabPane>
+                      <TabPane label="关注我的人" name="watched">
+                        <user-card
+                          userName="vczh"
+                          description="专业造轮子，拉黑抢前排。http://gaclib.net"
+                          :answer="17758"
+                          :watched="647935"
+                        ></user-card>
+                        <user-card
+                          userName="vczh"
+                          description="专业造轮子，拉黑抢前排。http://gaclib.net"
+                          :answer="17758"
+                          :watched="647935"
+                        ></user-card>
+                      </TabPane>
+                      <TabPane label="关注的问题" name="watchQuestion"></TabPane>
+                      <TabPane label="收藏的回答" name="collectedAnswer">
+                        <AnswerListCard avatar="h" name="ccc" qlink="" feed-title="热门内容,来自: 历史" question="历史上外交时有哪些尴尬场面？" answer="南海仲裁案之后，2016年7月16日，在美国国务院记者会上，凤凰卫视记者王冰汝向美国国务院发言人马克·托纳提问：“新加坡国立大学国际法中心在网站上刊登了地图和地名词典，其中一份地图，它的来源是美国政府，而且这张地图上写的是太平岛，而不是太平礁，这跟南海仲裁案“仲裁”结果不..."></AnswerListCard>
+                        <AnswerListCard qlink="" question="Mac 上的每个菜单命令，都能自定义快捷键吗?" cover-img="https://ss0.baidu.com/73F1bjeh1BF3odCf/it/u=3420588888,2752900295&fm=85&s=03809E4D4422EB430E34E03103008043" answer = "通常情况下，一个应用在 Mac 菜单栏显示的菜单命令，可以囊括了这个应用的大部分功能。同时也会为常用的菜单设置快捷键，比如最常见的「新建」功能，默认都是 Command + N。 为什么要自定义快捷键,许多开发者有自己的习惯,或..."></AnswerListCard>
+                        <AnswerListCard feed-title="你可能感兴趣: 生活"></AnswerListCard>
+                        <AnswerListCard
+                          v-for="(item, index) in answerList"
+                          :key="index"
+                          :avatar="item.avatar"
+                          :name="item.name" 
+                          :qlink="item.qlink" 
+                          :feed-title="item.feedTitle" 
+                          :question="item.question" 
+                          :answer="item.answer"
+                        >
+                        </AnswerListCard>
+                      </TabPane>
+                      <TabPane label="围观历史" name="history"></TabPane>
+                    </Tabs>
+                  </TabPane>
                 </Tabs>
               </div>
             </Col>
             <Col span="8" class="profile-body-right">
               <div>
                 <div class="watch-bar">
-                  <div class="watch-people">
+                  <div class="watch-people" @click="morePaneActiveName = 'watch';profilePaneActiveName = 'more';">
                     <div>关注了</div>
                     <div>8</div>
                   </div>
-                  <div class="watched-people">
+                  <div class="watched-people" @click="morePaneActiveName = 'watched';profilePaneActiveName = 'more';">
                     <div>关注者</div>
                     <div>5</div>
                   </div>
                 </div>
-                <div class="profile-lightItem">
+                <div class="profile-lightItem" @click="morePaneActiveName = 'watchQuestion';profilePaneActiveName = 'more';">
                   <span>
                     关注的问题
                   </span>
@@ -114,7 +176,7 @@
                     20
                   </span>
                 </div>
-                <div class="profile-lightItem">
+                <div class="profile-lightItem" @click="morePaneActiveName = 'collectedAnswer';profilePaneActiveName = 'more';">
                   <span>
                     收藏的回答
                   </span>
@@ -122,7 +184,7 @@
                     20
                   </span>
                 </div>
-                <div class="profile-lightItem">
+                <div class="profile-lightItem" @click="morePaneActiveName = 'history';profilePaneActiveName = 'more';">
                   <span>
                     围观历史
                   </span>
@@ -142,6 +204,7 @@
 <script>
   const TopBar = resolve => require(['@/components/topBar'], resolve)
   const AnswerListCard = resolve => require(['@/components/answerListCard'], resolve)
+  const userCard = resolve => require(['@/components/userCard'], resolve)
   import cookieManage from '@/mixins/cookieManage'
   import initInfo from '@/mixins/initInfo'
   export default {
@@ -149,7 +212,8 @@
     mixins: [cookieManage, initInfo],
     components: {
       TopBar,
-      AnswerListCard
+      AnswerListCard,
+      userCard
     },
     data() {
       return {
@@ -180,7 +244,9 @@
 						question:"历史上外交时有哪些尴尬场面？",
 						answer:"南海仲裁案之后，2016年7月16日，在美国国务院记者会上，凤凰卫视记者王冰汝向美国国务院发言人马克·托纳提问：“新加坡国立大学国际法中心在网站上刊登了地图和地名词典，其中一份地图，它的来源是美国政府，而且这张地图上写的是太平岛，而不是太平礁，这跟南海仲裁案“仲裁”结果不..."
 					}
-				]
+				],
+        morePaneActiveName: 'watch',
+        profilePaneActiveName: 'timeline'
       }
     },
     methods: {
@@ -191,6 +257,17 @@
             resolve();
           }, 2000);
         });
+      },
+      showWatchedQuestion() {
+        this.morePaneActiveName = 'watchQuestion'
+        this.profilePaneActiveName = 'more'
+      },
+      showCollectedAnswer() {
+        this.morePaneActiveName = 'watchQuestion'
+        this.profilePaneActiveName = 'more'
+      },
+      showHistory() {
+        console.log(3)
       }
     },
     created () {
@@ -257,6 +334,10 @@
             padding-left: 5px;
             margin: 0 15px;
           }
+          .morePane {
+            width: 95%;
+            margin: 0 auto;
+          }
         }
       }
       .profile-body-right{
@@ -296,6 +377,10 @@
           span:nth-child(2) {
             float: right;
           }
+          cursor: pointer;
+        }
+        .profile-lightItem:hover > span{
+          color:#175199 !important;
         }
         .profile-lightItem:last-child{
           border-bottom: 1px #dddee1 solid;
