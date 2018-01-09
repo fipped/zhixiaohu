7fde791135609ae81e0cbbabd308c1da31997975
flipped <823188494@qq.com>
2018-01-09 15:25:30 +0800

    *: fetch answers in home page

--- frontend/src/views/index.vue
+++ frontend/src/views/index.vue
@@ -11,15 +11,8 @@
 					 	<div class="feed-flow">
 							<AnswerListCard
 								v-for="(item, index) in answerList"
-								:key="index"
-								:avatar="item.avatar"
-								:name="item.name"
-                :pk="item.pk"
-								:feed-title="item.feedTitle" 
-								:coverImg="item.coverImg"
-								:question="item.question" 
-								:answer="item.answer"
-							>
+                :key="index"
+                :answer="item">
 							</AnswerListCard>
 						</div>			
 					</div>
@@ -45,61 +38,29 @@ export default {
   data() {
     return {
       windowHeight: "",
-      answerList: [
-        {
-          authorName: "ccc",
-          pk: "3",
-          feedTitle: "热门内容,来自: 历史",
-          question: "这个",
-          coverImg: "https://avatars3.githubusercontent.com/u/9395718?s=460&v=4",
-          answer:
-            "泻药.这个答案是一个示例答案.这个示例答案很长很长很长很长很长很长很长很长很长很长很长很长很长dd很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长很od"
-        },
-        {
-          authorName: "kiki",
-          pk: "4",
-          feedTitle: "热门内容,来自: 编程",
-          question: "作为一名程序员，我这属于什么水平？",
-          answer:
-            "南海仲裁案之后，2016年7月16日，在美国国务院记者会上，凤凰卫视记者王冰汝向美国国务院发言人马克·托纳提问：“新加坡国立大学国际法中心在网站上刊登了地图和地名词典，其中一份地图，它的来源是美国政府，而且这张地图上写的是太平岛，而不是太平礁，这跟南海仲裁案“仲裁”结果不很舒服卡仕"
-        },
-        {
-          authorName: "kiki",
-          pk: "4",
-          feedTitle: "热门内容,来自: 编程",
-          question: "作为一名程序员，我这属于什么水平？",
-          answer:
-            "南海仲裁案之后，2016年7月16日，在美国国务院记者会上，凤凰卫视记者王冰汝向美国国务院发言人马克·托纳提问：“新加坡国立大学国际法中心在网站上刊登了地图和地名词典，其中一份地图，它的来源是美国政府，而且这张地图上写的是太平岛，而不是太平礁，这跟南海仲裁案“仲裁”结果不..."
-        },
-        {
-          authorName: "kiki",
-          pk: "4",
-          feedTitle: "热门内容,来自: 编程",
-          question: "作为一名程序员，我这属于什么水平？",
-          answer:
-            "南海仲裁案之后，2016年7月16日，在美国国务院记者会上，凤凰卫视记者王冰汝向美国国务院发言人马克·托纳提问：“新加坡国立大学国际法中心在网站上刊登了地图和地名词典，其中一份地图，它的来源是美国政府，而且这张地图上写的是太平岛，而不是太平礁，这跟南海仲裁案“仲裁”结果不..."
-        },
-        {
-          authorName: "kiki",
-          pk: "4",
-          feedTitle: "热门内容,来自: 编程",
-		  question: "作为一名程序员，我这属于什么水平？",
-		  coverImg: "https://avatars3.githubusercontent.com/u/9395718?s=460&v=4",
-          answer:
-            "南海仲裁案之后，2016年7月16日，在美国国务院记者会上，凤凰卫视记者王冰汝向美国国务院发言人马克·托纳提问：“新加坡国立大学国际法中心在网站上刊登了地图和地名词典，其中一份地图，它的来源是美国政府，而且这张地图上写的是太平岛，而不是太平礁，这跟南海仲裁案“仲裁”结果不..."
-        },
-        {
-          authorName: "kiki",
-          pk: "4",
-          feedTitle: "热门内容,来自: 编程",
-          question: "作为一名程序员，我这属于什么水平？",
-          answer:
-            "南海仲裁案之后，2016年7月16日，在美国国务院记者会上，凤凰卫视记者王冰汝向美国国务院发言人马克·托纳提问：“新加坡国立大学国际法中心在网站上刊登了地图和地名词典，其中一份地图，它的来源是美国政府，而且这张地图上写的是太平岛，而不是太平礁，这跟南海仲裁案“仲裁”结果不..."
-        }
-      ]
+      answerList: {
+          type: Array,
+      }
     };
   },
   methods: {
+    fetchAnswerList() {
+      this.$http.get(`/api/answers/`)
+          .then(res => {
+            if(res.body.success==true) {
+             
+              this.answerList=res.body.data.results
+              console.log(this.answerList)
+            } else {
+              this.$Message.error(res.body.msg);
+            }
+          }, function(response){
+            // 响应错误回调 
+             this.err=true
+             this.errCode=response.status
+             this.errText=response.statusText
+          });
+    },
     handleReachBottom() {
       return new Promise(resolve => {
         setTimeout(() => {
@@ -110,6 +71,7 @@ export default {
     }
   },
   created() {
+    this.fetchAnswerList();
     if (this.initInfo()) {
       this.$router.push({ name: "home" });
     }
