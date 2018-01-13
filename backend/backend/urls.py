520abedddf9de7da0420bae02a08b136c516e381
lecoan <yangxiang@bupt.edu.cn>
2018-01-14 01:21:07 +0800

    re open redis

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -13,10 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+import threading
+
+import redis
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
+from api.utils.heat import HeatQueue
 from backend import settings
 
 urlpatterns = [
@@ -25,26 +29,26 @@ urlpatterns = [
 ]
 
 
-# def on_startup(r):
-#     string = r.get('hot')
-#     if string is not None:
-#         # print("hot string" + string.decode("utf-8"))
-#         HeatQueue.from_str(string.decode("utf-8"))
-#
-#
-# def time_task():
-#     string = HeatQueue.to_str()
-#     t = r.set('hot', string)
-#     # print(t)
-#     global timer
-#     timer = threading.Timer(10 * 60, time_task)
-#     timer.start()
-#     # print('finish timer task...........')
-#
-#
-# r = redis.StrictRedis(host='123.206.27.172', port=6379, password='canvasRedisPassvue')
-# on_startup(r)
-# timer = threading.Timer(5 * 10, time_task)
-# timer.start()
+def on_startup(r):
+    string = r.get('hot')
+    if string is not None:
+        # print("hot string" + string.decode("utf-8"))
+        HeatQueue.from_str(string.decode("utf-8"))
+
+
+def time_task():
+    string = HeatQueue.to_str()
+    t = r.set('hot', string)
+    # print(t)
+    global timer
+    timer = threading.Timer(10 * 60, time_task)
+    timer.start()
+    # print('finish timer task...........')
+
+
+r = redis.StrictRedis(host='123.206.27.172', port=6379, password='canvasRedisPassvue')
+on_startup(r)
+timer = threading.Timer(5 * 10, time_task)
+timer.start()
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
